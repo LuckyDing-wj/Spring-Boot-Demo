3375df7e9d211133e29653c820e6deb0cbe97ed7
Jie <18262281947@163.com>
2022-06-05 15:40:52 +0800

    ignore jersey for now

--- src/main/java/com/lucky/boot/config/JerseyConfig.java
+++ src/main/java/com/lucky/boot/config/JerseyConfig.java
@@ -1,17 +1,17 @@
 package com.lucky.boot.config;
 
-import com.lucky.boot.service.HelloService;
-import org.glassfish.jersey.server.ResourceConfig;
+//import com.lucky.boot.service.HelloService;
+//import org.glassfish.jersey.server.ResourceConfig;
 import org.springframework.context.annotation.Configuration;
 
 /**
  * @date 2022/5/31 15:59
  * @author: wj
  */
-@Configuration
-public class JerseyConfig extends ResourceConfig {
+//@Configuration
+public class JerseyConfig  {
 
-    public JerseyConfig() {
-        register(HelloService.class);
-    }
+//    public JerseyConfig() {
+//        register(HelloService.class);
+//    }
 }
--- src/main/java/com/lucky/boot/service/HelloService.java
+++ src/main/java/com/lucky/boot/service/HelloService.java
@@ -1,6 +1,11 @@
 package com.lucky.boot.service;
 
-import org.jvnet.hk2.annotations.Service;
+//import org.jvnet.hk2.annotations.Service;
+//
+//import javax.ws.rs.GET;
+//import javax.ws.rs.Path;
+//import javax.ws.rs.Produces;
+//import javax.ws.rs.QueryParam;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
@@ -11,18 +16,18 @@ import javax.ws.rs.QueryParam;
  * @date 2022/5/29 16:09
  * @author: wj
  */
-@Path("/service")
-@Service
+//@Path("/service")
+//@Service
 public class HelloService {
 
-    @GET
-    @Path("/cob")
-    @Produces("text/plain")
-    public String hello(@QueryParam("system")String system){
-        if (system == null || "".equalsIgnoreCase(system)){
-            system = "empty";
-        }
-        return "have system: " + system;
-    }
+//    @GET
+//    @Path("/cob")
+//    @Produces("text/plain")
+//    public String hello(@QueryParam("system")String system){
+//        if (system == null || "".equalsIgnoreCase(system)){
+//            system = "empty";
+//        }
+//        return "have system: " + system;
+//    }
 
 }
